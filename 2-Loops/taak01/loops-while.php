4b7c0dae48eb9dd8cf111c952345a6a27457bf35
BruceAmende <bruceamende32@gmail.com>
2020-06-08 15:37:25 +0200

    taak 2/01 klaar

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$leeftijd = 0;
+
+
+while($leeftijd < 18){
+    echo "<ul> Ik ben $leeftijd dus ik mag nog niet stemmen.</ul>";
+    $leeftijd++;
+}
+echo "Ik ben 18 jaar oud, dus ik heb stemrecht";
+
+
+
+
+
+?>
