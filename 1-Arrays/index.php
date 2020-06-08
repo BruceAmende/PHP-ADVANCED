29d89836f72994d897c63e8a7fd0264be10963c4
BruceAmende <bruceamende32@gmail.com>
2020-06-08 13:57:40 +0200

    taak 1/01 klaar

---
+++ 1-Arrays/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$piet = array ("voornaam" => "piet",
+        "achternaam" => "heijn",
+        "leeftijd" => 17,
+        "klas" => "9B" );
+
+echo "De naam is:" .  $piet['voornaam'],
+     "De leeftijd is:" . $piet['leeftijd'],
+     "De klas is:" . $piet['klas'];
+
+
+
+
+
+
+
+?>
