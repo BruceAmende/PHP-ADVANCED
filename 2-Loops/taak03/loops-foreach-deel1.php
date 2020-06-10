85857328ff155c1363b39866922b4b31bf22311e
BruceAmende <bruceamende32@gmail.com>
2020-06-10 14:39:54 +0200

    taak 2/03 klaar

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,16 @@
+<?php
+
+$dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+
+
+foreach($dagen as $value ){
+    echo "<ul>  $value </ul>";
+    
+}
+
+
+
+
+
+?>
