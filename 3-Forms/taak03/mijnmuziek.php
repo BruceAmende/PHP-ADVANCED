1038b39003f5a65981ccbcf31ae8daef44b45965
BruceAmende <bruceamende32@gmail.com>
2020-06-11 12:46:48 +0200

    taak 3/03 klaar

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo " Mijn favoriete" $_GET["nummer"] "is:" . $_GET["nummernaam"] . "De" $_GET["artiest"] "is:" . $_GET["artiestnaam"] . 
+     "Hij is" $_GET["uitgebracht"]  "op:" . $_GET["jaar"] "Het" $_GET["genre"] "is" . $_GET["genrenaam"];
+
+
+?>
+
+
+// http://localhost/OP_4%20Alleen%20bekijken/php-advanced/3-Forms/taak03/mijnmuziek.php?nummer=XO%20TOUR%20LIFE&artiest=Lil%20Uzi%20Vert&uitgebracht=2017&genre=hiphop//
