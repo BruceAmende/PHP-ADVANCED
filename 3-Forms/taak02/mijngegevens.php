94e88dcceee950733edaaf9c1065fbca5fd8e131
BruceAmende <bruceamende32@gmail.com>
2020-06-11 12:15:20 +0200

    taak 3/02 klaar

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn gegevens</title>
+</head>
+<body>
+
+
+
+
+
+<?php
+
+        echo "Voornaam is:" . $_POST["voornaam"];
+        echo "Achternaam is:" . $_POST["achternaam"];
+        echo "Klas is:" . $_POST["klas"];
+        echo "Leeftijd is:" . $_POST["leeftijd"];
+        echo "Adres is:" . $_POST["adres"];
+        echo "Plaatsnaam is:" . $_POST["pnaam"];
+        echo "Muziekband  is:" . $_POST["band"];
+        echo "Geslacht is:" . $_POST["gender"];
+
+
+
+        if ( isset($_GET['kleur']) ) {
+            print '<span class="' . $_GET['kleur'];
+        }
+
+
+?>
+
+
+    
+</body>
+</html>
